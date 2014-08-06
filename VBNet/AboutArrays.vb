9213fd21664348448c7b0cc004e3d3a7929b9db1
mark_van_peteghem <markvp@gmail.com>
2014-08-06 08:26:27 +0200

    Added koans about arrays for VB.

--- VBNet/AboutArrays.vb
+++ VBNet/AboutArrays.vb
@@ -2,39 +2,38 @@ Imports Xunit
 
 Public Class AboutArrays
     Inherits Koan
+
     <Koan(1)> _
     Public Sub CreatingArrays()
-        'Arrays can be created with New by adding () to the type, and with the elements between { }
+        'Arrays can be created by adding () to the variable name
         Dim empty_array = New Object() {}
-        'Note that the type of an Array is specified by its element type followed by ()
-        Assert.Equal(GetType(FillMeIn), empty_array.GetType())
-        'Note that you have to explicitly check for subclasses
-        Assert.True(GetType(Array).IsAssignableFrom(empty_array.GetType()))
+
         Assert.Equal(FILL_ME_IN, empty_array.Length)
     End Sub
 
     <Koan(2)> _
-    Public Sub ArrayLiterals()
-        'You don't have to specify a type if the arguments can be inferred
-        Dim array = {42,3,2,4}
-        Assert.Equal(GetType(Integer()), Array.GetType())
-        Assert.Equal(New Integer() {42}, Array)
-        'Are arrays 0-based or 1-based?
-        Assert.Equal(42, Array(CType(FILL_ME_IN, Integer)))
-        'This is important because...
-        Assert.True(Array.IsFixedSize)
-        '...it means we can't do this: array(4) = 13
-        Assert.Throws(GetType(FillMeIn), Sub() array(4) = 13)
-        'This is because the array is fixed at length 4.
+    Public Sub CreatingArraysWithASize()
+        ' You can also make it non-empty by adding the maximum index
+        Dim array(4) As Object
+        Assert.Equal(FILL_ME_IN, array.Length)
+        ' Note that arrays are 0-based:
+        array(0) = "abc"
+        array(4) = "xyz"
+        Assert.Equal(FILL_ME_IN, array(0))
+        Assert.Equal(FILL_ME_IN, array(4))
+        'This doesn't work:
+        'Assert.Equal(FILL_ME_IN, array(-1))
     End Sub
 
     <Koan(3)> _
-    Public Sub AccessingArrayElements()
-        Dim array = New String() {"peanut", "butter", "and", "jelly"}
-        Assert.Equal(FILL_ME_IN, Array(0))
-        Assert.Equal(FILL_ME_IN, Array(3))
-        'This doesn't work:
-        'Assert.Equal(FILL_ME_IN, array(-1))
+    Public Sub ArrayLiterals()
+        'You don't have to specify a type if the arguments can be inferred
+        Dim array = {42, 3, 2, 4}
+        Assert.Equal(New Integer() {42}, array)
+        Assert.True(array.IsFixedSize)
+        '...it means we can't do this: array(4) = 13
+        Assert.Throws(GetType(FillMeIn), Sub() array(4) = 13)
+        'This is because the array is fixed at length 4.
     End Sub
 
     <Koan(4)> _
@@ -42,5 +41,24 @@ Public Class AboutArrays
         Dim array = New String() {"peanut", "butter", "and", "jelly"}
         Assert.Equal(New String() {FILL_ME_IN, FILL_ME_IN}, array.Take(2).ToArray())
         Assert.Equal(New String() {FILL_ME_IN, FILL_ME_IN}, array.Skip(1).Take(2).ToArray())
+    End Sub
+
+    <Koan(5)> _
+    Public Sub ResizingWithReDim()
+        ' You can make an array bigger or smaller
+        Dim array = {42, 3, 2, 4}
+        ReDim array(6)
+        Assert.Equal(FILL_ME_IN, array.Length)
+        ' but the values are not kept
+        Assert.Equal(FILL_ME_IN, array(0))
+    End Sub
+
+    <Koan(6)> _
+    Public Sub ResizingWithReDimAndPreserve()
+        ' Adding Preserve keeps the values
+        Dim array = {42, 3, 2, 4}
+        ReDim Preserve array(6)
+        Assert.Equal(FILL_ME_IN, array.Length)
+        Assert.Equal(FILL_ME_IN, array(0))
     End Sub
 End Class
