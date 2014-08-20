f85a26cbf8fdf581edb4982a30b6a17ef3352d36
mark_van_peteghem <markvp@gmail.com>
2014-08-20 16:29:21 +0200

    Add koans about asynchrony.

--- KoanRunner/Program.cs
+++ KoanRunner/Program.cs
@@ -6,69 +6,71 @@ using System.Reflection;
 using System.Text;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 namespace DotNetKoans.KoanRunner
 {
-	class Program
-	{
-		static bool aKoanHasFailed = false;
+    class Program
+    {
+        static bool aKoanHasFailed = false;
         static int numberKoansProcessed = 0;
         static int firstFailingKoan = -1;
         static List<int> progress = new List<int>();
         const string progressFilename = "progress";
 
-		static int Main(string[] args)
-		{
-			try
-			{
-				Console.WriteLine("");
-				Console.WriteLine("");
-				Console.WriteLine("*******************************************************************");
-				Console.WriteLine("*******************************************************************");
+        static int Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("*******************************************************************");
+                Console.WriteLine("*******************************************************************");
 
                 ReadProgress();
 
-				string koan_path = args[0];
-				Xunit.ExecutorWrapper wrapper = new ExecutorWrapper(koan_path, null, false);
-				System.Reflection.Assembly koans = System.Reflection.Assembly.LoadFrom(koan_path);
-				if (koans == null) { Console.WriteLine("Bad Assembly"); return -1; }
-				Type pathType = null;
-				foreach (Type type in koans.GetExportedTypes())
-				{
-					if (typeof(KoanHelpers.IAmThePathToEnlightenment).IsAssignableFrom(type))
-					{
-						pathType = type;
-						break;
-					}
-				}
+                string koan_path = args[0];
+                Xunit.ExecutorWrapper wrapper = new ExecutorWrapper(koan_path, null, false);
+                System.Reflection.Assembly koans = System.Reflection.Assembly.LoadFrom(koan_path);
+                if (koans == null) { Console.WriteLine("Bad Assembly"); return -1; }
+                Type pathType = null;
+                foreach (Type type in koans.GetExportedTypes())
+                {
+                    if (typeof(KoanHelpers.IAmThePathToEnlightenment).IsAssignableFrom(type))
+                    {
+                        pathType = type;
+                        break;
+                    }
+                }
 
-				KoanHelpers.IAmThePathToEnlightenment path = Activator.CreateInstance(pathType) as KoanHelpers.IAmThePathToEnlightenment;
-				string thePath = path.ThePath;
+                KoanHelpers.IAmThePathToEnlightenment path = Activator.CreateInstance(pathType) as KoanHelpers.IAmThePathToEnlightenment;
+                string thePath = path.ThePath;
                 string[] theKoanNames = KoanNames;
 
                 foreach (string koanName in theKoanNames)
-				{
-					Run(thePath+"."+koanName, koans, wrapper);
-				}
+                {
+                    Run(thePath + "." + koanName, koans, wrapper);
+                }
 
                 Console.WriteLine("{0}", Encouragement());
                 progress.Add(firstFailingKoan);
                 WriteProgress();
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine("Karma has killed the runner. Exception was: " + ex.ToString());
-				return -1;
-			}
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Karma has killed the runner. Exception was: " + ex.ToString());
+                return -1;
+            }
             if (!aKoanHasFailed)
                 firstFailingKoan = numberKoansProcessed;
-			Console.WriteLine("Koan progress: {0}/{1}", firstFailingKoan, numberKoansProcessed);
-			Console.WriteLine("*******************************************************************");
-			Console.WriteLine("*******************************************************************");
-			Console.WriteLine("");
-			Console.WriteLine("");
-			return 0;
-		}
+            Console.WriteLine("Koan progress: {0}/{1}", firstFailingKoan, numberKoansProcessed);
+            Console.WriteLine("*******************************************************************");
+            Console.WriteLine("*******************************************************************");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            return 0;
+        }
 
         static void ReadProgress()
         {
@@ -97,7 +99,7 @@ namespace DotNetKoans.KoanRunner
             for (int i = progress.Count - 1; i >= 0; --i)
                 if (progress[i] == firstFailingKoan)
                     ++failedAttempts;
-            if (failedAttempts>=3)
+            if (failedAttempts >= 3)
                 return "I sense frustration. Do not be afraid to ask for help.";
             if (failedAttempts >= 1)
                 return "Do not lose hope.";
@@ -123,20 +125,18 @@ namespace DotNetKoans.KoanRunner
             return filename + '(' + lineNr + ')';
         }
 
-        static string Run(string className, System.Reflection.Assembly koanAssembly, ExecutorWrapper wrapper)
+        static void Run(string className, System.Reflection.Assembly koanAssembly, ExecutorWrapper wrapper)
         {
-            
             Type classToRun = koanAssembly.GetType(className);
 
             if (classToRun == null)
             {
                 Console.WriteLine("Class {0} not found", className);
-                return "(0/0)";
             }
 
             object koans = Activator.CreateInstance(classToRun);
 
-            string[] queue = new string[classToRun.GetMethods().Length + 1];
+            MethodInfo[] queue = new MethodInfo[classToRun.GetMethods().Length + 1];
             int highestKoanNumber = 0;
             foreach (MethodInfo method in classToRun.GetMethods())
             {
@@ -145,32 +145,49 @@ namespace DotNetKoans.KoanRunner
                 if (custAttr == null) { continue; }
                 if (queue[custAttr.Position] != null)
                     Console.WriteLine("More than one koan in {0} has the position {1}", className, custAttr.Position);
-                queue[custAttr.Position] = method.Name;
+                queue[custAttr.Position] = method;
                 if (custAttr.Position > highestKoanNumber) { highestKoanNumber = custAttr.Position; }
             }
 
             int numberOfKoansRunInThisCollection = 0;
-			int numberOfKoansPassedInThisCollection = 0;
+            int numberOfKoansPassedInThisCollection = 0;
 
             BindingFlags flags = BindingFlags.InvokeMethod | BindingFlags.Instance | BindingFlags.Public;
-            foreach (string test in queue)
+            foreach (MethodInfo test in queue)
             {
-                if (String.IsNullOrEmpty(test)) 
-					continue;
+                if (test == null)
+                    continue;
 
                 ++numberKoansProcessed;
                 numberOfKoansRunInThisCollection++;
                 if (aKoanHasFailed)
-					continue;
+                    continue;
                 try
                 {
-                    classToRun.InvokeMember(test, flags, null, koans, new Object[] { });
+                    Type attType = typeof(AsyncStateMachineAttribute);
+
+                    // Obtain the custom attribute for the method. 
+                    // The value returned contains the StateMachineType property. 
+                    // Null is returned if the attribute isn't present for the method. 
+                    var attrib = (AsyncStateMachineAttribute)test.GetCustomAttribute(attType);
+
+                    if (attrib != null)
+                    {
+                        Task t = (classToRun.InvokeMember(test.Name, flags, null, koans, new Object[] { }) as Task);
+                        t.Wait();
+                    }
+                    else
+                    {
+                        classToRun.InvokeMember(test.Name, flags, null, koans, new Object[] { });
+                    }
                 }
                 catch (Exception e)
                 {
                     aKoanHasFailed = true;
-                    firstFailingKoan = numberKoansProcessed-1;
+                    firstFailingKoan = numberKoansProcessed - 1;
                     Exception ie = e.InnerException;
+                    if (ie == null)
+                        ie = e;
                     string st = ie.StackTrace;
                     string[] stLines = st.Split(new char[] { '\n' });
                     // uncomment if there are problems parsing the stack trace:
@@ -192,18 +209,17 @@ namespace DotNetKoans.KoanRunner
                     // In both the Error List pane and Output pane clicking it jumps to the line where a fix is needed.
                     // Replacing 'warning' by 'error' would work too, but also give a distracting extra error message.
                     Console.WriteLine(filenameAndLine + ": warning: {0}", message);
-                    
+
                 }
-				if (!aKoanHasFailed)
-					numberOfKoansPassedInThisCollection++;
-			}
+                if (!aKoanHasFailed)
+                    numberOfKoansPassedInThisCollection++;
+            }
 
             if (numberOfKoansRunInThisCollection != highestKoanNumber)
             {
                 Console.WriteLine("!!!!WARNING - Some Koans in {0} appear disabled. The highest koan found was {1} but we ran {2} koan(s)",
                     className, highestKoanNumber, numberOfKoansRunInThisCollection);
             }
-			return string.Format("({0}/{1})", numberOfKoansPassedInThisCollection, numberOfKoansRunInThisCollection);
         }
 
         static public string[] KoanNames
@@ -224,7 +240,8 @@ namespace DotNetKoans.KoanRunner
 				"AboutDelegates",
 				"AboutLambdas",
                 "AboutUsing",
-                "AboutSymbols"
+                "AboutSymbols",
+                "AboutAsynchrony"
                 };
             }
         }
